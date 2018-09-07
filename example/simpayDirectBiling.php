3d49dfa9118c5f84a5b305f9569f4b6666967722
Rafał Więcek <gitzzz14@gmail.com>
2018-09-07 18:54:38 +0200

    Update example

--- example/simpayDirectBiling.php
+++ example/simpayDirectBiling.php
@@ -1,27 +1,35 @@
 <?php
 include( 'SimPayDB.php' );
-include( '../database.php' );
+include( 'database.php' );
 
 $simPay = new SimPayDB();
 
+//Ustawianie API Key z panelu usługi partnera
 $simPay -> setApiKey( 'lNEEDQPfPKHleZdd' );
 
+//Parsowanie informacji pobranych z POST
 if( $simPay -> parse( $_POST ) ){
-
+	
+	//Sprawdzenie czy parsowanie przebiegło pomyslnie
 	if( $simPay -> isError() ){
 		$simPay -> okTransaction();
 
 		return;
 	}
-
+	
+	//Dodanie informacji o transakcji do bazy danych
+	//$simPay -> getStatus() - Obecny status transakcji
+	//$simPay -> getValuePartner() - Ile partner rzeczywiście uzyskał prowizji
+	//$simPay -> getControl() - Wartość control wysłana przy starcie transakcji
 	$stmt = $pdoObject -> prepare( 'INSERT INTO `direct_billing`( `status`, `value`, `control`) VALUES ( :status , :value , :control )' );
 
 	$stmt -> bindValue( ':status' , $simPay -> getStatus(), PDO::PARAM_STR );
-	$stmt -> bindValue( ':value' , $simPay -> getValue(), PDO::PARAM_STR );
+	$stmt -> bindValue( ':value' , $simPay -> getValuePartner(), PDO::PARAM_STR );
 	$stmt -> bindValue( ':control' , $simPay -> getControl(), PDO::PARAM_STR );
 		 
 	$stmt -> execute();
-
+	
+	//Sprawdzenie czy transakcja została opłacona
 	if( $simPay -> isTransactionPaid() ){
 		$stmt = $pdoObject -> prepare('SELECT * FROM `users` WHERE `id` = :user_id');
 	
@@ -32,6 +40,7 @@ if( $simPay -> parse( $_POST ) ){
 		$detailsUser = $stmt -> fetchAll();
 
 		if( count( $detailsUser ) == 0 ){
+			//Zwrócenie że transakcja została pomyślnie odebrana przez partnera
 			$simPay -> okTransaction();
 
 			return;
@@ -40,21 +49,23 @@ if( $simPay -> parse( $_POST ) ){
 		$stmt = $pdoObject -> prepare('INSERT INTO `recharg_history`( `user_id`, `amount`, `type` ) VALUES ( :user_id , :amount , 5 )');
 		
 		$stmt -> bindValue( ':user_id' , $detailsUser[ 0 ][ 'id' ] , PDO::PARAM_INT );
-		$stmt -> bindValue( ':amount' , $simPay -> getValue() , PDO::PARAM_STR );
+		$stmt -> bindValue( ':amount' , $simPay -> getValuePartner() , PDO::PARAM_STR );
 
 		$stmt -> execute();
 
 		$stmt = $pdoObject -> prepare('UPDATE `users` SET `money`= `money` + :amount WHERE `id` = :user_id');
 		
 		$stmt -> bindValue( ':user_id' , $detailsUser[ 0 ][ 'id' ] , PDO::PARAM_INT );
-		$stmt -> bindValue( ':amount' , $simPay -> getValue() , PDO::PARAM_STR );
+		$stmt -> bindValue( ':amount' , $simPay -> getValuePartner() , PDO::PARAM_STR );
 
 		$stmt -> execute();
 	}
 }
 else{
+	//Sprawdzenie typu błedu
 	error_log( $simPay -> getErrorText() );
 }
 
+//Zwrócenie że transakcja została pomyślnie odebrana przez partnera
 $simPay -> okTransaction();
 ?>
