0daa5baad6c9a8bb7ea374af6bea245a6f7af53d
Rafal Wiecek <gitzzz14@gmail.com>
2019-06-13 22:41:31 +0100

    Update SimPayDB.php
    Dodanie opcji generowanie transakcji server side ( Klasa SimPayDBTransaction )
    
    Dodano tryb debug setDebugMode( true ) logujący błędy w error_logach oraz printujący je.

--- src/SimPayDB.php
+++ src/SimPayDB.php
@@ -16,29 +16,61 @@ class SimPayDB{
 
 	private $userNumber = '';
 
+	protected $debugMode = false;
+
+	public function setDebugMode( $value ){
+		$this -> debugMode = (boolean)$value;
+	}
+
+	private function isDebugMode(){
+		return !!$this -> debugMode;
+	}
+
+	private function logDebugMode( $err ){
+		print_r( $err ); 
+
+		error_log( $err );
+	}
+
 	public function parse( $data ){
 
 		if( !isset( $data[ 'id' ] ) ){
 			$this -> setError( true , 1 );
+
+			if( $this -> isDebugMode() ){
+				$this -> logDebugMode( $this -> getErrorText() );
+			}
 			
 			return false;
 		}
 
 		if( !isset( $data[ 'status' ] ) ){
 			$this -> setError( true , 1 );
+
+			if( $this -> isDebugMode() ){
+				$this -> logDebugMode( $this -> getErrorText() );
+			}
 			
 			return false;
 		}
 		
 		if( !isset( $data[ 'valuenet' ] ) ){
 			$this -> setError( true , 1 );
+
+			if( $this -> isDebugMode() ){
+				$this -> logDebugMode( $this -> getErrorText() );
+			}
 			
 			return false;
 		}
 
 		if( !isset( $data[ 'sign' ] ) ){
 			$this -> setError( true , 1 );
-			
+
+			if( $this -> isDebugMode() ){
+				$this -> logDebugMode( $this -> getErrorText() );
+			}
+
 			return false;
 		}
 
@@ -60,6 +92,10 @@ class SimPayDB{
 
 		if( hash( 'sha256' , $this -> transId . $this -> status . $this -> value . $this -> valuePartner . $this -> control . $this -> apiKey ) != $data[ 'sign' ] ){
 			$this -> setError( true , 3 );
+
+			if( $this -> isDebugMode() ){
+				$this -> logDebugMode( $this -> getErrorText() );
+			}
 			
 			return false;
 		}
@@ -68,12 +104,20 @@ class SimPayDB{
 
 		if( $this -> value <= 0.00 ){
 			$this -> setError( true , 4 );
+
+			if( $this -> isDebugMode() ){
+				$this -> logDebugMode( $this -> getErrorText() );
+			}
 		}
 
 		$this -> valuePartner = floatval( str_replace( ',' , '.' , $this -> valuePartner ) );
 
 		if( $this -> valuePartner <= 0.00 ){
 			$this -> setError( true , 4 );
+
+			if( $this -> isDebugMode() ){
+				$this -> logDebugMode( $this -> getErrorText() );
+			}
 		}
 
 		return true;
@@ -158,39 +202,177 @@ class SimPayDB{
 			return 0.00;
 		}
 
-		$arrayComission = [];
+		$arrayCommission = [];
 
 		switch( $provider ){
 			case 1:{
-				$arrayComission = [ 0.65 , 0.65 , 0.65 ];
+				$arrayCommission = [ 0.65 , 0.65 , 0.65 ];
 
 				break;
 			}
 			case 2:{
-				$arrayComission = [ 0.55 , 0.65 , 0.70 ];
+				$arrayCommission = [ 0.55 , 0.65 , 0.70 ];
 
 				break;
 			}
 			case 3:{
-				$arrayComission = [ 0.60 , 0.60 , 0.60 ];
+				$arrayCommission = [ 0.60 , 0.60 , 0.60 ];
 
 				break;
 			}
 			case 4:{
-				$arrayComission = [ 0.50 , 0.50 , 0.50 ];
+				$arrayCommission = [ 0.50 , 0.50 , 0.50 ];
 
 				break;
 			}
 		}
 
 		if( $amount < 9 ){
-			return number_format( $amount * $arrayComission[ 0 ] , 2 , '.' , '' );
+			return number_format( $amount * $arrayCommission[ 0 ] , 2 , '.' , '' );
 		}
 		else if( $amount < 25 ){
-			return number_format(  $amount * $arrayComission[ 1 ] , 2 , '.' , '' );
+			return number_format(  $amount * $arrayCommission[ 1 ] , 2 , '.' , '' );
 		}
 		else{
-			return number_format(  $amount * $arrayComission[ 2 ] , 2 , '.' , '' );
+			return number_format(  $amount * $arrayCommission[ 2 ] , 2 , '.' , '' );
+		}
+	}
+}
+
+class SimPayDBTransaction{
+	protected $requestOptions = [
+		'serviceId' => NULL,
+		'amount' => NULL,
+		'amount_gross' => NULL,
+		'amount_required' => NULL,
+		'provider' => NULL,
+		'control' => NULL,
+		'failure' => NULL,
+		'complete' => NULL,
+		'sign' => NULL
+	];
+
+	protected $apiKey = '';
+
+	protected $resultInstance = NULL;
+
+	protected $debugMode = false;
+
+	public function setDebugMode( $value ){
+		$this -> debugMode = (boolean)$value;
+	}
+
+	private function isDebugMode(){
+		return !!$this -> debugMode;
+	}
+
+	private function logDebugMode( $err ){
+		print_r( $err ); 
+
+		error_log( $err );
+	}
+
+	public function setServiceID( $id ){
+		$this -> requestOptions[ 'serviceId' ] = $id;
+	}
+
+	public function setAmount( $amount ){
+		$this -> requestOptions[ 'amount' ] = $amount;
+
+		$this -> requestOptions[ 'amount_gross' ] = NULL;
+		$this -> requestOptions[ 'amount_required' ] = NULL;
+	}
+
+	public function setAmountGross( $amount ){
+		$this -> requestOptions[ 'amount' ] = NULL;
+
+		$this -> requestOptions[ 'amount_gross' ] = $amount;
+		$this -> requestOptions[ 'amount_required' ] = NULL;
+	}
+
+	public function setAmountRequired( $amount ){
+		$this -> requestOptions[ 'amount' ] = NULL;
+
+		$this -> requestOptions[ 'amount_gross' ] = NULL;
+		$this -> requestOptions[ 'amount_required' ] = $amount;
+	}
+
+	public function setProvider( $provider ){
+		$this -> requestOptions[ 'provider' ] = $provider;
+	}
+
+	public function setControl( $control ){
+		$this -> requestOptions[ 'control' ] = $control;
+	}
+
+	public function setFailureLink( $link ){
+		$this -> requestOptions[ 'failure' ] = $link;
+	}
+
+	public function setCompleteLink( $link ){
+		$this -> requestOptions[ 'complete' ] = $link;
+	}
+
+	public function setApiKey( $key ){
+		$this -> apiKey = $key;
+	}
+
+	protected function generateSign(){
+		if( $this -> requestOptions[ 'amount' ] ){
+			$hash = hash( 'sha256' , $this -> requestOptions[ 'serviceId' ] . $this -> requestOptions[ 'amount' ] . $this -> requestOptions[ 'control' ] . $this -> apiKey );
+		}
+		else if( $this -> requestOptions[ 'amount_gross' ] ){
+			$hash = hash( 'sha256' , $this -> requestOptions[ 'serviceId' ] . $this -> requestOptions[ 'amount_gross' ] . $this -> requestOptions[ 'control' ] . $this -> apiKey );
+		}
+		else if( $this -> requestOptions[ 'amount_required' ] ){
+			$hash = hash( 'sha256' , $this -> requestOptions[ 'serviceId' ] . $this -> requestOptions[ 'amount_required' ] . $this -> requestOptions[ 'control' ] . $this -> apiKey );
+		}
+
+		return $hash;
+	}
+
+	public function getTransactionLink(){
+		if( !isset( $this -> resultInstance -> link ) ){
+			return '';
+		}
+
+		return $this -> resultInstance -> link;
+	}
+
+	public function getResults(){
+		return $this -> resultInstance;
+	}
+
+	public function generateTransaction(){
+
+		$this -> requestOptions[ 'sign' ] = $this -> generateSign();
+
+		$ch = curl_init('https://simpay.pl/db/api');
+
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLINFO_HEADER_OUT, true);
+		curl_setopt($ch, CURLOPT_POST, true);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, $this -> requestOptions );
+
+		$result = curl_exec($ch);
+
+		curl_close($ch);
+
+		try{
+			$resultDecode = json_decode( $result );
+			
+			$this -> resultInstance = $resultDecode;
+		}
+		catch( Exception $err ){
+			if( $this -> isDebugMode() ){
+				$this -> logDebugMode( $err );
+			}
+		}
+
+		if( $this -> resultInstance != 'success' ){
+			if( $this -> isDebugMode() ){
+				$this -> logDebugMode( $this -> resultInstance -> message );
+			}
 		}
 	}
 }
