45de7fdb7de8434b86e31b46067b13de6ec8f6c7
kgrzelak <k.grzelak@simpay.pl>
2019-06-17 22:00:15 +0200

    Dodanie nowych przykladów
    * Dodanie nowego sposobu generowania płatności DirectCarrierBilling
    * Poprawa czytelności kodu
    * Poprawa czytelności konfiguracji
    * Dodanie generowania płatności PHP-CURL

---
+++ examples/generate/pdo.php
@@ -0,0 +1,91 @@
+<?php
+require_once('SimPayDB.php');
+
+$cfg = array(
+	'mysql' => array(
+		'host' => 'localhost',
+		'username' => '3306',
+		'password' => 'password',
+		'database' => 'database'
+	),
+	'simpay' => array(
+		/*
+			Tryb debugowania
+			Typ pola bolean true/false
+			Opis Ustawienie pola na TRUE, włączna tryb debugowania, który wyświetla błędy np. w konfiguracji.
+		*/
+		'debugMode' => FALSE,
+		/*
+			ID usługi
+			Typ pola int, np. 60
+			Opis ID Usługi DirectCarrierBilling z panelu simpay.pl, 
+		*/
+		'serviceId' => 1,
+		/*
+			Klucz API usługi
+			Typ pola string
+		*/
+		'apiKey' => 'lNEEDQPfPKHleZdd',
+		/*
+			Control
+			Typ pola string
+		*/
+		'control' => 112,
+		/*
+			Adres URL do powrotu po prawidłowej transakcji
+			Typ pola string
+			Opis Użytkownik jest przekierowywany na ten adres po prawidłowo zakończonej transakcji.
+		*/
+		'completeUrl' => 'https://api.systemy.net/simpay/complete.php',
+		/*
+			Adres URL do powrotu po nieudanej transakcji
+			Typ pola string
+			Opis Użytkownik jest przekierowywany na ten adres po nieprawidłowo zakończonej transakcji.
+		*/
+		'failureUrl' => 'https://api.systemy.net/simpay/failure.php',
+		/*
+			Kwota transakcji
+			Typ pola float
+		*/
+		'amount' => 10.00
+	)
+);
+
+$pdo = null;
+
+//Laczenie do bazy danych
+try {
+	$pdo = new PDO('mysql:host=' . $cfg['mysql']['host'] . ';dbname=' . $cfg['mysql']['database'] . ';port=3306', $cfg['mysql']['username'], $cfg['mysql']['password'], array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8mb4'"));
+	$pdo->query('SET NAMES utf8mb4');
+	$pdo->query('SET CHARACTER SET utf8mb4');
+	$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $e) {
+	exit('Connection error.');
+}
+
+$stmt = $pdo->prepare("INSERT INTO `dcb`(`control`, `price`, `status`) VALUES (:control, :price, 'new');");
+$stmt->bindValue(':control', $cfg['simpay']['control'], PDO::PARAM_STR);
+$stmt->bindValue(':price', $cfg['simpay']['price'], PDO::PARAM_STR);
+$stmt->execute();
+
+
+$simpayTransaction = new SimPayDBTransaction();
+$simpayTransaction->setDebugMode($cfg['simpay']['debugMode']);
+$simpayTransaction->setServiceID($cfg['simpay']['serviceId']);
+$simpayTransaction->setApiKey($cfg['simpay']['apiKey']);
+$simpayTransaction->setControl($cfg['simpay']['control']);
+$simpayTransaction->setCompleteLink($cfg['simpay']['completeUrl']);
+$simpayTransaction->setFailureLink($cfg['simpay']['failureUrl']);
+//$simpayTransaction->setAmount(10);
+//$simpayTransaction->setAmountGross(10);
+$simpayTransaction->setAmountRequired($cfg['simpay']['amount']);
+$simpayTransaction->generateTransaction();
+
+if ($simpayTransaction->getResults()->status == "success") {
+	/*
+		Tutaj należy przekierować użytkownika używając np. header('Location: ' . $simpayTransaction->getResults()->link);
+	*/
+	echo $simpayTransaction->getResults()->link;
+} else {
+	echo 'Generowanie transakcji nie powiodło się!';
+}
---
+++ examples/ipn/mysqli_ob.php
@@ -0,0 +1,78 @@
+<?php
+require_once('SimPayDB.php');
+
+$cfg = array(
+	'mysql' => array(
+		'host' => 'localhost',
+		'username' => 'username',
+		'password' => 'password',
+		'database' => 'database'
+	),
+	'simpay' => array(
+		/*
+			Klucz API usługi
+			Typ pola string
+		*/
+		'apiKey' => 'lNEEDQPfPKHleZdd',
+	)
+);
+
+//Laczenie do bazy danych
+$mysqli = new mysqli($cfg['mysql']['host'], $cfg['mysql']['username'], $cfg['mysql']['password'], $cfg['password']['database']);
+if ($mysqli->connect_error) {
+    exit('Connection error: ' . $mysqli->connect_error);
+} 
+
+$simPay = new SimPayDB();
+
+$simPay->setApiKey($cfg['simpay']['apiKey']);
+
+//Parsowanie informacji pobranych z POST
+if ($simPay->parse($_POST)) {
+	
+	//Sprawdzenie czy parsowanie przebiegło pomyslnie
+	if ($simPay->isError()) {
+		//Zwrócenie że transakcja została pomyślnie odebrana przez partnera
+		$simPay->okTransaction();
+		$mysqli->close();
+		exit();
+	}
+	
+	//Dodanie informacji o transakcji do bazy danych
+	//$simPay->getStatus() - Obecny status transakcji
+	//$simPay->getValuePartner() - Ile partner rzeczywiście uzyskał prowizji
+	//$simPay->getControl() - Wartość control wysłana przy starcie transakcji
+	
+	//Sprawdzenie czy transakcja została opłacona
+	if ($simPay->isTransactionPaid()) {
+		$stmt = $mysqli->prepare("SELECT * FROM `dcb` WHERE `control` = ?;");
+	
+		$stmt->bind_param($simPay->getControl());
+		$stmt->execute();
+
+		$detailsUser = $stmt->fetch(); 
+
+		if (count($detailsUser) == 0) {
+			//Zwrócenie że transakcja została pomyślnie odebrana przez partnera
+			$simPay->okTransaction();
+
+			$stmt = $mysqli->prepare("UPDATE `dcb` SET `status` = 'completed', `amount` = ? WHERE `control` = ?;");
+			$stmt->bind_param($simPay->getValuePartner(), $simPay->getControl());
+			$stmt->execute();
+			$mysqli->close();
+			
+			exit();
+		}
+		
+	}
+} else {
+	//Sprawdzenie typu błedu
+	error_log($simPay->getErrorText());
+}
+
+//Zwrócenie że transakcja została pomyślnie odebrana przez partnera
+//Wartość zwracana przez partnera powinna zawierać tylko "OK". System SimPay uzna wtedy, że transakcja została poprawnie obsłużona i nie będzie ponawiał zapytań do serwisu partnera.
+$simPay->okTransaction();
+
+$mysqli->close();
+?>
